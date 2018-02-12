ed8e57d23232f4d4163b859ef2f8310c24511b1d
Yusuf Iqbal <ysf.iqbl@gmail.com>
2018-02-12 15:04:42 +0530

    Added app config.

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.url = 'http://localhost:8090/';
+exports.import_url = 'http://localhost:8090/pages/worddav/uploadimport.action?pageId=65598';
+exports.import_page_title = '';
+exports.credentials = {
+	username: 'admin',
+	password: 'password'
+}
